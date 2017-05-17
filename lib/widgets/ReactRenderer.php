396dcfac359edb075bd4f203ee316031c00d1223
Bohdan <bogdantokman@gmail.com>
2017-05-17 12:13:06 +0300

    hot fix

--- lib/widgets/ReactRenderer.php
+++ lib/widgets/ReactRenderer.php
@@ -129,7 +129,7 @@ class ReactRenderer extends Widget
 
         // Creates the markup of the component
         if ($options['prerender'] === true) {
-            $markup = $this->getReact()->setComponent($this->componentsSourceJs, $this->props)->getMarkup();
+            $markup = $this->react->setComponent($this->componentsSourceJs, $this->props)->getMarkup();
         }
 
         // Pass props back to view as value of `data-react-props`
